14747135eb268c96b167e583da1bdc057b82776f
Matthew <metju312@gmail.com>
2017-06-24 01:57:44 +0200

    working map with polyline

--- src/main/webapp/app/entities/activity/activity-detail.controller.js
+++ src/main/webapp/app/entities/activity/activity-detail.controller.js
@@ -5,14 +5,13 @@
         .module('moveApp')
         .controller('ActivityDetailController', ActivityDetailController);
 
-    ActivityDetailController.$inject = ['$scope', '$rootScope', '$stateParams', 'previousState', 'entity', 'Activity', 'Node', 'User'];
+    ActivityDetailController.$inject = ['$scope', '$rootScope', '$stateParams', 'previousState', 'entity', 'Activity', 'Node', 'User', '$http'];
 
-    function ActivityDetailController($scope, $rootScope, $stateParams, previousState, entity, Activity, Node, User) {
+    function ActivityDetailController($scope, $rootScope, $stateParams, previousState, entity, Activity, Node, User, $http) {
         var vm = this;
 
         vm.activity = entity;
         vm.previousState = previousState.name;
-
 
 
         var unsubscribe = $rootScope.$on('moveApp:activityUpdate', function(event, result) {
@@ -20,21 +19,54 @@
         });
         $scope.$on('$destroy', unsubscribe);
 
+
         initMap();
 
         function initMap() {
-            var myLatLng = {lat: 52.252778, lng: 20.899918};
+            $http({
+            method:'GET',
+            url:"/api/nodes/activity/" + vm.activity.id})
+            .then(function (response) {
+                vm.nodes = response.data;
+                var destinations = [];
+                var length = vm.nodes.length;
+                var myLatLng = null;
+                var sumX = 0;
+                var sumY = 0;
+                if (length == 1) {
+                    myLatLng = {lat: 52.252778, lng: 20.899918};/*wat*/
+                } else {
+                    var node = null;
+                    for (var i = 0; i < length; i++) {
+                        node = vm.nodes[i];
+                        destinations.push(new google.maps.LatLng(node.latitude, node.longitude));
+                        sumX+=node.latitude;
+                        sumY+=node.longitude;
+                    }
+                    var polylineOptions = {path: destinations, strokeColor:"#16aae9", strokeWeight:3};
+                    var polyline = new google.maps.Polyline(polylineOptions);
+                    myLatLng = {lat: sumX/length, lng: sumY/length}
+                }
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 15,
+                    center: myLatLng
+                });
 
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 15,
-                center: myLatLng
+                if (length != 0) {
+                    polyline.setMap(map);
+                }
+                /*marker - show center*/
+                // var marker = new google.maps.Marker({
+                //     position: myLatLng,
+                //     map: map,
+                //     title: 'Hello World!'
+                // });
+            }, function (reason) {
+                $scope.error = reason;
             });
 
-            var marker = new google.maps.Marker({
-                position: myLatLng,
-                map: map,
-                title: 'Hello World!'
-            });
+
+
         }
     }
 })();
