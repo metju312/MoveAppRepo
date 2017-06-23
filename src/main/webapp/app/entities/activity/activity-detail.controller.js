b1a3c9a9569ba8f7a2e36a45e65470e628206539
Matthew <metju312@gmail.com>
2017-06-23 21:55:06 +0200

    map in angular works

--- src/main/webapp/app/entities/activity/activity-detail.controller.js
+++ src/main/webapp/app/entities/activity/activity-detail.controller.js
@@ -13,9 +13,28 @@
         vm.activity = entity;
         vm.previousState = previousState.name;
 
+
+
         var unsubscribe = $rootScope.$on('moveApp:activityUpdate', function(event, result) {
             vm.activity = result;
         });
         $scope.$on('$destroy', unsubscribe);
+
+        initMap();
+
+        function initMap() {
+            var myLatLng = {lat: 52.252778, lng: 20.899918};
+
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 15,
+                center: myLatLng
+            });
+
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                title: 'Hello World!'
+            });
+        }
     }
 })();
